dc3d7171fa0c81827f3e409754439544c9d18f2d
Jani Basha Shaik <janibashashaik917@gmail.com>
2026-07-28 22:17:31 +0530

    Add initial data for Yahoo Finance API
    Inserted initial data for Yahoo Finance API into ctl_source_system table.

--- src/sql/control/001_create_ctl_source_system.sql
+++ src/sql/control/001_create_ctl_source_system.sql
@@ -14,3 +14,32 @@ CREATE OR REPLACE TABLE ctl_source_system
     modified_date TIMESTAMP,
     modified_by STRING
 );
+
+GO
+
+
+INSERT INTO ctl_source_system (
+    source_id,
+    source_name,
+    source_code,
+    source_category,
+    source_type,
+    base_url,
+    auth_type,
+    description,
+    is_active,
+    created_date,
+    created_by
+) VALUES (
+    1,
+    'Yahoo Finance API',
+    'YFINANCE',
+    'Financial Market Data',
+    'Python API Web Scraper',
+    'https://yahoo.com',
+    'None',
+    'Public stock market data ingestion using the yfinance library',
+    true,
+    current_timestamp(),
+    'SparkSQL_User'
+);
